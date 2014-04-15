97facba45ad17df556f7d0ecd130c57f8ba22fe5
Michael <michael@Michaels-MacBook-Pro.local>
2014-04-15 20:35:37 +0800

    - POJ 1276, turn bounded knapsack to 0-1 knapsack, but “Time Limit Exceeded”

--- KnapSack/main.cpp
+++ KnapSack/main.cpp
@@ -8,8 +8,9 @@
 
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <cstring>
 
-//存储元素的结构
 typedef struct item
 {
     int weight;
@@ -21,23 +22,35 @@ int main()
 {
     int weight=10;
     int itemnum=4;
-    
-    item items[4]={{6,30},{3,14},{4,16},{2,9}};
-    
-    int* m = new int[weight+1];
-    memset(m, 0, weight+1);
-    for (int i = 1; i <= itemnum; i++) {
-        for (int w = weight; w > 0; w--) {
-            if (w >= items[i-1].weight) {
-                m[w] = max(m[w], m[w-items[i-1].weight]+items[i-1].values);
+    int N;
+    while (!(cin >> weight).fail()) {
+        cin >> N;
+        int* n = new int[N];
+        int* D = new int[N];
+        
+        vector<item> items;
+        for (int i = 0; i < N; i++) {
+            cin >> n[i] >> D[i];
+            for (int j = 0; j < n[i]; j++) {
+                items.push_back({D[i], D[i]});
             }
         }
+        itemnum = (int)items.size();
+        
+        int* m = new int[weight+1];
+        memset(m, 0, weight+1);
+        for (int i = 1; i <= itemnum; i++) {
+            for (int w = weight; w > 0; w--) {
+                if (w >= items[i-1].weight) {
+                    m[w] = max(m[w], m[w-items[i-1].weight]+items[i-1].values);
+                }
+            }
+        }
+        cout << m[weight];
+        cout << endl;
     }
-    for (int i = 0; i <= weight; i++) {
-        cout << m[i] << " ";
-    }
-    cout<<endl;
     
+    //    item items[4]={{6,30},{3,14},{4,16},{2,9}};
     //    int (*m)[11] = new int[5][11];
     //    memset(m, 0, (itemnum+1)*(weight+1));
     
