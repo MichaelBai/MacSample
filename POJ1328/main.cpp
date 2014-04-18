5de811e342eb17f039cd99ba5a9a8ade0f030960
Michael <michael@Michaels-MacBook-Pro.local>
2014-04-19 00:47:37 +0800

    Initial Commit

---
+++ POJ1328/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  POJ1328
+//
+//  Created by Michael on 4/19/14.
+//  Copyright (c) 2014 MichaelBai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
