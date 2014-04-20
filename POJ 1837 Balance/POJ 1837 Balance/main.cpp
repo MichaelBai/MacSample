5e3afb91f0e59ccbaaeef2bd3356a0b7edf33a75
MichaelBai <michael@Michaels-MacBook-Pro.local>
2014-04-20 12:38:41 +0800

    POJ 1837 Balance, DP

---
+++ POJ 1837 Balance/POJ 1837 Balance/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  POJ 1837 Balance
+//
+//  Created by Michael on 4/20/14.
+//  Copyright (c) 2014 MichaelBai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
