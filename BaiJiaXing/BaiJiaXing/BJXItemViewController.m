8ca6277c6ad0725171faed2e57b4066870145692
MichaelBai <michael@Michaels-MacBook-Pro.local>
2014-06-16 19:26:17 +0800

    - add swipe back in itemViewController

--- BaiJiaXing/BaiJiaXing/BJXItemViewController.m
+++ BaiJiaXing/BaiJiaXing/BJXItemViewController.m
@@ -8,16 +8,40 @@
 
 #import "BJXItemViewController.h"
 
+@interface BJXItemViewController ()
+
+- (void) backViewGesture:(UIPanGestureRecognizer *)sender;
+
+@end
+
 @implementation BJXItemViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     [self.webView setBackgroundColor:[UIColor lightGrayColor]];
+    // customize webcontent
     NSMutableString* content = [NSMutableString stringWithString:self.webContent];
     [content insertString:@"<p style=\"overflow:hidden;\">" atIndex:0];
     [content appendString:@"</p>"];
     [self.webView loadHTMLString:content baseURL:nil];
+    
+    //Set gesture
+    UIPanGestureRecognizer *backViewGestureRecognizer
+    = [[UIPanGestureRecognizer alloc] initWithTarget:self
+                                              action:@selector(backViewGesture:)];
+    
+    [self.view addGestureRecognizer:backViewGestureRecognizer];
+}
+
+- (void) backViewGesture:(UIPanGestureRecognizer *)sender
+{
+    CGPoint translation = [sender translationInView:sender.view];
+    //Threshold : distance which how long move your thumb.
+    if (translation.x > 45.0) {
+        [self.navigationController popViewControllerAnimated:YES];
+        [self.view removeGestureRecognizer:sender];
+    }
 }
 
 @end
